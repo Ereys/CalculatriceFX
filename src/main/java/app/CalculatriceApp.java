9d2143539bcc0908f3d4bd2828901f44b9e1b0fd
Pierre Ribollet <46523223+Ereys@users.noreply.github.com>
2023-02-21 10:22:23 +0100

    first commit

---
+++ src/main/java/app/CalculatriceApp.java
@@ -0,0 +1,16 @@
+package app;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class CalculatriceApp extends Application {
+
+    public static void main(String[] args){
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.calculatricefx {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.calculatricefx to javafx.fxml;
+    exports com.example.calculatricefx;
+}
